015116819adeae3b5a59a8c5f9c19af191316455
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-04-29 17:33:40 -0700

    Small naming change

--- GraphBindingMotifMutationCounts.R
+++ GraphBindingMotifMutationCounts.R
@@ -3,6 +3,7 @@ library(data.table)
 countsFilePath = choose.files(multi = FALSE)
 countsTable = fread(countsFilePath)
 countsDataName = strsplit(basename(countsFilePath),'.', fixed = T)[[1]][1]
+countsDataName = strsplit(countsDataName, "aggregate_", fixed = T)[[1]][2]
 
 barplot(countsTable[[2]], names.arg = countsTable[[1]], xlab = "Motif Position",
         ylab = "Normalized Counts", main = paste(countsDataName, "(Motif Strand)"))
