85f09cca29887ed07e057eaf6b5505f9710c3080
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-06-02 16:06:32 -0700

    Added "Sloppy Copy" option
    Adds a new default behavior where the nucleosome map is loaded into memory as a dictionary, using the same key as the encompassing feature ODS.  This way, rows can be matched exactly from the quartile file instead of just reconstructing the nucleosome position from the quartile file (i.e. the sloppy copy.)

--- python_scripts/GetQuartileNucleosomePositions.py
+++ python_scripts/GetQuartileNucleosomePositions.py
@@ -1,7 +1,5 @@
 # This script takes a nucleosome positions file and the upper and lower quartile nucleosomes from H1 density data 
 # (or other data, I suppose), and creates new nucleosome positioning files containing only those lower/upper quartile nucleosomes.
-# NOTE: This script doesn't actually check to see if the nucleosome positions in the quartile files are present in the
-#       base nucleosome file.  It just converts them to bed format.  This has the potential to cause pRoBlEmS.
 from mutperiodpy.Tkinter_scripts.TkinterDialog import TkinterDialog
 from mutperiodpy.helper_scripts.UsefulBioinformaticsFunctions import parseFastaDescription
 from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import checkDirs, getDataDirectory
@@ -9,8 +7,21 @@ import os, subprocess
 from typing import List
 
 
-def getQuartileNucleosomePositions(quartileFilePaths: List[str], nucPosDir: str, stratificationType):
+# NOTE: If sloppyCopy is true, this function doesn't actually check to see if the nucleosome positions in the quartile files are present in the
+#       base nucleosome file.  It just converts them to bed format.  This has the potential to cause pRoBlEmS.
+def getQuartileNucleosomePositions(quartileFilePaths: List[str], nucPosDir: str, stratificationType, sloppyCopy):
     
+    # If this isn't just a sloppy copy, create a dictionary containing each line in the root nucPos file for the corresponding location ID
+    # Maybe this could cause memory issues, but I think it should be fine since the nucleosome maps are usually not too big.
+    if not sloppyCopy:
+        nucPosLines = dict()
+        with open(os.path.join(nucPosDir,os.path.basename(nucPosDir)+".bed")) as nucPosFile:
+
+            for line in nucPosFile:
+                chromosome, startPos, endPos = line.split()[:3]
+                key = (chromosome + ':' + str(float(startPos)) + '-' + str(float(endPos) - 1) + "(+)")
+                nucPosLines[key] = line
+
     for quartileFilePath in quartileFilePaths:
 
         print("Working in",os.path.basename(quartileFilePath))
@@ -34,9 +45,11 @@ def getQuartileNucleosomePositions(quartileFilePaths: List[str], nucPosDir: str,
                 # Parse out the location information and convert it to bed format.
                 for line in quartileFile:
 
-                    chromosome, startPos, endPos, strand = parseFastaDescription(line.split('\t')[0])
-
-                    outputNucPosFile.write('\t'.join((chromosome, startPos, str(float(endPos) + 1), '.', '.', strand)) + '\n')
+                    if sloppyCopy:
+                        chromosome, startPos, endPos, strand = parseFastaDescription(line.split()[0])
+                        outputNucPosFile.write('\t'.join((chromosome, startPos, str(float(endPos) + 1), '.', '.', strand)) + '\n')
+                    else:
+                        outputNucPosFile.write(nucPosLines[line.split()[0]])
 
         # Sort the output
         subprocess.run(("sort","-k1,1","-k2,3n",outputNucPosFilePath,"-o",outputNucPosFilePath), check = True)
@@ -49,6 +62,7 @@ def main():
     dialog.createMultipleFileSelector("Quartile Files:",0, "quartile.tsv", ("Tab Separated Files",".tsv"))    
     dialog.createFileSelector("Nucleosome Directory:",1, directory=True)
     dialog.createDropdown("Stratification Type", 2, 0, ["h1 density", "other"])
+    dialog.createCheckbox("Sloppy Copy?", 3, 0)
 
     # Run the UI
     dialog.mainloop()
@@ -57,7 +71,7 @@ def main():
     if dialog.selections is None: quit()
 
     getQuartileNucleosomePositions(dialog.selections.getFilePathGroups()[0], dialog.selections.getIndividualFilePaths()[0],
-                                   dialog.selections.getDropdownSelections()[0].replace(' ', '_'))
+                                   dialog.selections.getDropdownSelections()[0].replace(' ', '_'), dialog.selections.getToggleStates()[0])
 
 
 if __name__ == "__main__": main()
