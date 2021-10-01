dc4a90e86d65e52ca73c75abd2aea217ebd56553
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-10-01 15:52:27 -0700

    Incorporate more flanking and color domain data
    Utilizes changes to the CountThisInThat system from benbiohelpers to bin with multiple flanking regions and stratify by chromatin domain (color).

--- python_scripts/BinInGenes.py
+++ python_scripts/BinInGenes.py
@@ -7,16 +7,21 @@ from benbiohelpers.CountThisInThat.Counter import ThisInThatCounter
 from benbiohelpers.CountThisInThat.OutputDataStratifiers import AmbiguityHandling
 
 
-def binInGenes(featureFilePaths: List[str], geneDesignationsFilePath, flankingBinSize = 0, filePathSuffix = ""):
+def binInGenes(featureFilePaths: List[str], geneDesignationsFilePath, flankingBinSize = 0, flankingBinNum = 0, 
+               filePathSuffix = "", colorColIndex = None):
 
     class BinInGenesCounter(ThisInThatCounter):
 
         def setUpOutputDataHandler(self):
             super().setUpOutputDataHandler()
-            self.outputDataHandler.addFeatureFractionStratifier(outputName = "Gene_Fraction", flankingBinSize = flankingBinSize)
+            if colorColIndex is not None:
+                self.outputDataHandler.addSimpleEncompassingColStratifier(outputName = "Color_Domain", colIndex = colorColIndex)
+            self.outputDataHandler.addFeatureFractionStratifier(outputName = "Gene_Fraction", flankingBinSize = flankingBinSize, 
+                                                                flankingBinNum = flankingBinNum)
             self.outputDataHandler.addStrandComparisonStratifier(strandAmbiguityHandling = AmbiguityHandling.tolerate)
-            self.outputDataHandler.createOutputDataWriter(self.outputFilePath, customStratifyingNames=(None, {True:"Coding_Strand_Counts", 
-                                                                                                            False:"Noncoding_Strand_Counts"}))
+            if colorColIndex is None: customStratifyingNames=(None, {True:"Coding_Strand_Counts", False:"Noncoding_Strand_Counts"})
+            else: customStratifyingNames=(None, None, {True:"Coding_Strand_Counts", False:"Noncoding_Strand_Counts"})
+            self.outputDataHandler.createOutputDataWriter(self.outputFilePath, customStratifyingNames = customStratifyingNames)
 
     for featureFilePath in featureFilePaths:
 
@@ -36,14 +41,16 @@ def main():
     dialog = TkinterDialog(workingDirectory=getDataDirectory())
     dialog.createMultipleFileSelector("Feature Files (e.g. mutations):",0,"bin_me.bed",("Bed Files",".bed"))    
     dialog.createFileSelector("Gene Designations:",1,("Bed Files",".bed"))
+    dialog.createCheckbox("Color Domain is present in 7th (index=6) column",2, 0)
 
-    flankDialog = dialog.createDynamicSelector(2, 0)
+    flankDialog = dialog.createDynamicSelector(3, 0)
     flankDialog.initCheckboxController("Gene designations include flanking regions")
     flankSizeDialog = flankDialog.initDisplay(True, "FlankSize")
     flankSizeDialog.createTextField("Flanking bin size:", 0, 0, defaultText="0")
+    flankSizeDialog.createTextField("Flanking bin number:", 1, 0, defaultText = "0")
     flankDialog.initDisplayState()
 
-    fileSuffixDialog = dialog.createDynamicSelector(3, 0)
+    fileSuffixDialog = dialog.createDynamicSelector(4, 0)
     fileSuffixDialog.initCheckboxController("Custom file suffix")
     suffixDialog = fileSuffixDialog.initDisplay(True, "Suffix")
     suffixDialog.createTextField("File Suffix:", 0, 0, defaultText="all_genes")
@@ -55,14 +62,21 @@ def main():
     # If no input was received (i.e. the UI was terminated prematurely), then quit!
     if dialog.selections is None: quit()
 
-    if flankDialog.getControllerVar(): flankSize = int(dialog.selections.getTextEntries("FlankSize")[0])
-    else: flankSize = 0
+    if dialog.selections.getToggleStates()[0]: colorColIndex = 6
+    else: colorColIndex = None
+
+    if flankDialog.getControllerVar(): 
+        flankBinSize = int(dialog.selections.getTextEntries("FlankSize")[0])
+        flankBinNum = int(dialog.selections.getTextEntries("FlankSize")[1])
+    else: 
+        flankBinSize = 0
+        flankBinNum = 0
 
     if fileSuffixDialog.getControllerVar(): fileSuffix = dialog.selections.getTextEntries("Suffix")[0]
     else: fileSuffix = ""
 
     binInGenes(dialog.selections.getFilePathGroups()[0], dialog.selections.getIndividualFilePaths()[0],
-               flankSize, fileSuffix)
+               flankBinSize, flankBinNum, fileSuffix, colorColIndex)
 
 
 if __name__ == "__main__": main()
